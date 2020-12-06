1b5195734badae455833e78d85302b6b1fae3b0b
vishwajeetiitb <vishwajeet724728@gmail.com>
2020-12-06 23:21:16 +0530

    test

--- conscientious_reactive/firebase_test.py
+++ conscientious_reactive/firebase_test.py
@@ -1,9 +1,16 @@
 from firebase import firebase
+import time
 firebase = firebase.FirebaseApplication('https://patrolling-7f86a-default-rtdb.firebaseio.com/')
 data = {
 	'name' : 'vishwajeet bhagywant',
 	'Email' : 'vishwajeet724728@gmail.com',
 	'phone' : 8766543498
 }
-results = firebase.put('/cars','yo',4)
-print(results)
+a = 0
+for i in range(20):
+	tic = time.time()
+	results = firebase.put('/cars','yo',4)
+	toc = time.time()
+	a +=toc-tic
+	# print(a)
+print(a/20)
