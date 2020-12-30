1791248b2a0b7b7604a1c93afad1f9b232e471fb
Dikshuy <dikshant1212hsr@gmail.com>
2020-12-30 12:18:24 +0530

    ideal vs practical code added

---
+++ conscientious_reactive/ideal_vs_practical/data/observations.md
@@ -0,0 +1,57 @@
+**dual failure**: both car node and node node communcation failure but on the same node. 
+
+corner node: node 0
+
+edge node: node 2
+
+center node: node 12
+
+# 1 car:
+
+in comparison to proper connection, almost same
+
+>corner node is disconnected: behavior changes slightly in few cases
+
+>edge node in disconnected: randomness comes into play 
+
+>center node is disconnected: behavior remains same
+
+# 2 cars:
+
+in comparison to proper connection, similar
+
+>corner node is disconnected: asymmetrical behavior 
+
+>edge node in disconnected: asymmetrical behavior 
+
+>center node is disconnected: asymmetrical behavior
+
+# 4 cars:
+
+in comparison to proper connection, similar
+
+>corner node is disconnected: asymmetrical behavior with large variations in few cases
+
+>edge node in disconnected: almost identical
+
+>center node is disconnected: almost identical
+
+# 6 cars:
+
+in comparison to proper connection, similar
+
+>corner node is disconnected: asymmetrical with slight variations
+
+>edge node in disconnected: very slight variation but almost identical
+
+>center node is disconnected: very slight variation but almost identical
+
+# 10 cars:
+
+in comparison to proper connection, similar
+
+>corner node is disconnected: very slight variation but almost identical
+
+>edge node in disconnected: very slight variation but almost identical
+
+>center node is disconnected: very slight variation but almost identical
--- conscientious_reactive/ideal_vs_practical/standard_deviation_graphs.py
+++ conscientious_reactive/ideal_vs_practical/standard_deviation_graphs.py
@@ -3,30 +3,48 @@ import matplotlib.pyplot as plt
 import xlsxwriter
 import numpy as np
 import os
-rootdir ='../dual_failure/data/'
+rootdir1 ='/data/'
+rootdir2 ='/no_dead_data/'
 
 cars = [1,2,4,6,10]
 deads = [0,2,12]
 num_runs = 5
 for dead in deads:
-	avg = []
-	std = []
+	avg1 = []
+	avg2 = []
+	# std = []
 	for car in cars:
-		path = rootdir+'cr'+str(car)+'/'+str(dead)+'dead/'
-		runs = []
-		instantaneous_node_idleness =[]
+		path1 = rootdir1+'cr'+str(car)+'/'+str(dead)+'dead/'
+		path2 = rootdir2
+		runs1 = []
+		runs2 = []
+		instantaneous_node_idleness1 = []
+		instantaneous_node_idleness2 = []
 		for i in range(num_runs):
-			runs.append(np.array(pd.read_excel(pd.ExcelFile(path+'run'+str(i)+'/run.xlsx'))))
-			runs[i] = runs[i][5000:][:,1:]
-			instantaneous_node_idleness.append(np.mean(runs[i],axis=1))
-		graph_idlness = np.mean(instantaneous_node_idleness,axis=1)
-		avg.append(np.mean(graph_idlness))
-		std.append(np.std(graph_idlness))
-	avg = np.array(avg)
-	std = np.array(std)
+			runs1.append(np.array(pd.read_excel(pd.ExcelFile(path1+'run'+str(i)+'/run.xlsx'))))
+			runs1[i] = runs1[i][5000:][:,1:]
+			instantaneous_node_idleness1.append(np.mean(runs1[i],axis=1))
+		runs2.append(np.array(pd.read_excel(pd.ExcelFile(path2+'/'+str(car)+'car_run.xlsx'))))
+		runs2[i] = runs2[i][5000:][:,1:]
+		instantaneous_node_idleness2.append(np.mean(runs2[i],axis=1))
+		graph_idlness1 = np.mean(instantaneous_node_idleness1,axis=1)
+		graph_idlness2 = np.mean(instantaneous_node_idleness2,axis=1)
+		avg1.append(np.mean(graph_idlness1))
+		avg2.append(np.mean(graph_idlness2))
+		# std.append(np.std(graph_idlness))
+	#avg = np.array(avg)
+	#std = np.array(std)
+	avg1 = np.array(avg1)
+	avg2 = np.array(avg2)
 	plt.figure()
-	plt.errorbar(cars, avg, xerr=0.0,yerr=std, elinewidth=1, markeredgewidth=1)
-	plt.show()
+	# plt.errorbar(cars, avg,yerr=std, ecolor='g', capthick=1.0)
+	plt.plot(cars. avg1)
+	plt.plot(cars, avg2)
+	plt.title("standard deviation of idleness with varying runs and agents")
+	plt.xlabel("number of agents")
+	plt.ylabel("graph idleness")
+	# plt.show()
+	plt.savefig('cmparison with dead'+str(dead)+'.png')
 	
 
 
