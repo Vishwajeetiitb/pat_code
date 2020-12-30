d417b3c817ca799e40a558db53459a10b36c470c
vishwajeetiitb <vishwajeet724728@gmail.com>
2020-12-30 14:52:24 +0530

    plots added

--- conscientious_reactive/ideal_vs_practical/standard_deviation_graphs.py
+++ conscientious_reactive/ideal_vs_practical/standard_deviation_graphs.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import xlsxwriter
 import numpy as np
 import os
-rootdir1 ='/data/'
-rootdir2 ='/no_dead_data/'
+rootdir1 ='./data/'
+rootdir2 ='./no_dead_data/'
 
 cars = [1,2,4,6,10]
 deads = [0,2,12]
@@ -24,9 +24,9 @@ for dead in deads:
 			runs1.append(np.array(pd.read_excel(pd.ExcelFile(path1+'run'+str(i)+'/run.xlsx'))))
 			runs1[i] = runs1[i][5000:][:,1:]
 			instantaneous_node_idleness1.append(np.mean(runs1[i],axis=1))
-		runs2.append(np.array(pd.read_excel(pd.ExcelFile(path2+'/'+str(car)+'car_run.xlsx'))))
-		runs2[i] = runs2[i][5000:][:,1:]
-		instantaneous_node_idleness2.append(np.mean(runs2[i],axis=1))
+		runs2.append(np.array(pd.read_excel(pd.ExcelFile(path2+str(car)+'car_run.xlsx'))))
+		runs2 = runs2[0][5000:][:,1:]
+		instantaneous_node_idleness2.append(np.mean(runs2,axis=1))
 		graph_idlness1 = np.mean(instantaneous_node_idleness1,axis=1)
 		graph_idlness2 = np.mean(instantaneous_node_idleness2,axis=1)
 		avg1.append(np.mean(graph_idlness1))
@@ -34,17 +34,20 @@ for dead in deads:
 		# std.append(np.std(graph_idlness))
 	#avg = np.array(avg)
 	#std = np.array(std)
-	avg1 = np.array(avg1)
-	avg2 = np.array(avg2)
+	#avg1 = np.array(avg1)
+	#avg2 = np.array(avg2)
 	plt.figure()
+	print(avg1)
+	print(avg2)
 	# plt.errorbar(cars, avg,yerr=std, ecolor='g', capthick=1.0)
-	plt.plot(cars. avg1)
-	plt.plot(cars, avg2)
-	plt.title("standard deviation of idleness with varying runs and agents")
+	plt.plot(cars, avg1,label = 'practical')
+	plt.plot(cars, avg2, label = 'ideal')
+	plt.legend()
+	plt.title("ideal vs practical")
 	plt.xlabel("number of agents")
 	plt.ylabel("graph idleness")
 	# plt.show()
-	plt.savefig('cmparison with dead'+str(dead)+'.png')
+	plt.savefig('comparison with dead'+str(dead)+'.png')
 	
 
 
