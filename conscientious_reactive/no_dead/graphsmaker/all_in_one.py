f6ef0886f458c448ee621ffd5b30a038495b75c2
vishwajeetiitb <vishwajeet724728@gmail.com>
2021-01-10 17:37:39 +0530

    scam

--- conscientious_reactive/no_dead/graphsmaker/all_in_one.py
+++
@@ -1,9 +0,0 @@
-import os
-import time 
-scripts = ['get_peaks.py','split_runs.py','pattern_graphs.py','get_starting.py','starting_graphs.py','grid_graphs.py']
-tic  = time.time()
-for element in scripts:
-    print(element+' started')
-    os.system('python3 '+element)
-    print(element+' ended')
- 
--- conscientious_reactive/no_dead/graphsmaker/create_folder.py
+++
@@ -1,5 +0,0 @@
-import os
-f = open("./path_name.txt", "r")
-path = './data/' + f.read() +'individual/'
-os.system('rm -rf '+path)
-os.system('mkdir '+path)
