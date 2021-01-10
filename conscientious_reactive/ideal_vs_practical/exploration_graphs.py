09412a66d5030532efad0989161f3ba7c1ef2e37
Dikshuy <dikshant1212hsr@gmail.com>
2021-01-11 01:59:15 +0530

    exploration codes added

--- conscientious_reactive/ideal_vs_practical/exploration_graphs.py
+++ conscientious_reactive/ideal_vs_practical/exploration_graphs.py
@@ -8,16 +8,16 @@ rootdir2 ='./no_dead_data/'
 
 cars = [1,2,4,6,10]
 deads = [0,2,12]
+no_deads = [26]
 num_runs = 5
+avg = []
+avg2 = []
 for dead in deads:
 	avg1 = []
-	avg2 = []
 	for car in cars:
 		path1 = rootdir1+'cr'+str(car)+'/'+str(dead)+'dead/'
-		path2 = rootdir2
 		runs1 = []
-		runs2 = []
-		run_exploration_time1, run_exploration_time2 =[], []
+		run_exploration_time1 = []
 			
 		for i in range(num_runs):
 			node_list1 = [i for i in range(25)]
@@ -31,25 +31,33 @@ for dead in deads:
 						node_list1.remove(visited_node1)
 				if len(node_list1)==0:
 					run_exploration_time1.append(step[0])
-					break
-		node_list2 = [i for i in range(25)]
-		runs2.append(np.array(pd.read_excel(pd.ExcelFile(path2+str(car)+'car_run.xlsx'))))
-		runs2 = runs2[0][0:][:,:]
-		for step in runs2:
-			visited_node2 = np.where(step[1:] == 0)[0]
-			if len(visited_node2) is not 0:
-				visited_node2 = visited_node2[0]
-				if visited_node2 in node_list2:
-					node_list2.remove(visited_node2)
-			if len(node_list2)==0:
-				run_exploration_time2.append(step[0])
-				break
-		print(run_exploration_time2)		
-		avg2.append(np.mean(run_exploration_time2))
+					break		
 		avg1.append(np.mean(run_exploration_time1))
-  
+		avg.append(avg1)
+for dead in no_deads:
+	for car in cars:
+		path2 = rootdir2+'cr'+str(car)+'/'+str(dead)+'dead/'
+		runs2 = []
+		run_exploration_time2 = []
+			
+		for i in range(num_runs):
+			node_list2 = [i for i in range(25)]
+			runs2.append(np.array(pd.read_excel(pd.ExcelFile(path2+'run'+str(i)+'/run.xlsx'))))
+			runs2[i] = runs2[i][0:][:,:]
+			for step in runs2[i]:
+				visited_node2 = np.where(step[1:] == 0)[0]
+				if len(visited_node2) is not 0:
+					visited_node2 = visited_node2[0]
+					if visited_node2 in node_list2:
+						node_list2.remove(visited_node2)
+				if len(node_list2)==0:
+					run_exploration_time2.append(step[0])
+					break
+		avg2.append(np.mean(run_exploration_time2))
+
+for dead in deads: 
 	plt.figure()
-	plt.plot(cars, avg1, label = "practical")
+	plt.plot(cars, avg[dead], label = "practical")
 	plt.plot(cars, avg2, label = "ideal")
 	plt.legend()
 	# plt.errorbar(cars, avg,yerr=std, fmt = 'o', ecolor='g', capthick=1.0)
