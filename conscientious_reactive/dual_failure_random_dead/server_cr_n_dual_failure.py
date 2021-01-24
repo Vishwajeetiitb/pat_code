2814b2c54d9325e8d9194e28bd6181f81f94ab78
vishwajeetiitb <vishwajeet724728@gmail.com>
2021-01-24 08:11:50 +0530

    keys isssues resolved

--- conscientious_reactive/dual_failure_random_dead/server_cr_n_dual_failure.py
+++ conscientious_reactive/dual_failure_random_dead/server_cr_n_dual_failure.py
@@ -180,7 +180,7 @@ def run(env):
     ma_ga=deque(maxlen=3000)
     gav=[]
     ss=[]
-    num_steps = 40
+    num_steps = 4000
     cloud_array = np.zeros([25,cars,25,1])
     idle_2d = np.zeros([num_steps, 25])
     while traci.simulation.getMinExpectedNumber()>0:
