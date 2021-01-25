3a0ae5a7ae23bf34a077b36401039aa0a8d220b8
Dikshuy <dikshant1212hsr@gmail.com>
2021-01-25 10:29:14 +0530

    change

--- conscientious_reactive/dual_failure_random_dead/graphsmaker/standard_deviation_graphs.py
+++ conscientious_reactive/dual_failure_random_dead/graphsmaker/standard_deviation_graphs.py
@@ -5,14 +5,14 @@ import numpy as np
 import os
 rootdir ='../data/'
 
-cars = [1,2,4,6,10]
-deads = [0,2,12]
-num_runs = 5
+cars = [1,2,3,4,5,6,7,8,9,10]
+deads = [0,2,4,6,8]
+num_runs = 10
 for dead in deads:
 	avg = []
 	std = []
 	for car in cars:
-		path = rootdir+'cr'+str(car)+'/'+str(dead)+'dead/'
+		path = rootdir+'cr'+str(car)+'/'+str(dead)+'devices_failed/'
 		runs = []
 		instantaneous_node_idleness =[]
 		for i in range(num_runs):
@@ -25,14 +25,15 @@ for dead in deads:
 		std.append(np.std(graph_idlness))
 	#avg = np.array(avg)
 	#std = np.array(std)
+	print(avg)
 	plt.figure()
 	plt.plot(cars, avg, 'b--')
 	plt.errorbar(cars, avg,yerr=std,  fmt='o', ecolor='g', capthick=1.0)
 	plt.title("standard deviation of idleness with varying runs and agents")
 	plt.xlabel("number of agents")
 	plt.ylabel("graph idleness")
-	# plt.show()
-	plt.savefig('dead'+str(dead)+'_new.png', dpi = 100)
+	plt.show()
+	# plt.savefig('dead'+str(dead)+'_new.png', dpi = 100)
 	
 
 
