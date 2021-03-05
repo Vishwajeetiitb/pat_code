d394d0cd7a34b99606215afefcabe4371680ac9f
vishwajeetiitb <vishwajeet724728@gmail.com>
2021-03-05 13:11:08 +0530

    std done

--- conscientious_reactive/dual_failure_random_dead/graphsmaker/standard_deviation_graphs.py
+++ conscientious_reactive/dual_failure_random_dead/graphsmaker/standard_deviation_graphs.py
@@ -9,16 +9,15 @@ cars = [1,2,3,4,5,6,7,8,9,10,12]
 deads = [0]#,5,12,20,25]
 num_runs = 8
 plt.figure()
-
+avg = []
+std = []
 for dead in deads:
-	avg = []
-	std = []
 	for car in cars:
 		path = rootdir+'cr'+str(car)+'/'+str(dead)+'devices_failed/'
 		runs = []
 		instantaneous_node_idleness =[]
 		for i in range(num_runs):
-			print(path+'run'+str(i)+'/run.xlsx')
+			#print(path+'run'+str(i)+'/run.xlsx')
 			runs.append(np.array(pd.read_excel(pd.ExcelFile(path+'run'+str(i)+'/run.xlsx'))))
 			runs[i] = runs[i][500:][:,1:]
 			instantaneous_node_idleness.append(np.mean(runs[i],axis=1))
@@ -26,17 +25,17 @@ for dead in deads:
 		avg.append(np.mean(graph_idlness))
 		std.append(np.std(graph_idlness))
 	print(std)
-	plt.plot(cars, std)#, label ="no of device failures ="+str(dead))
+	#plt.errorbar(car, avg,yerr=std,  fmt='o', ecolor='g', capthick=1.0)
 	plt.draw()
 
-# plt.plot(cars, avg, 'b--')
+plt.plot(cars, avg, 'b--')
 # for i in range(len(avgs)):
 # 	plt.plot(cars, avgs[i], label ="no of device failures ="+str(i*2))
-# plt.errorbar(cars, avg,yerr=std,  fmt='o', ecolor='g', capthick=1.0)
+plt.errorbar(cars, avg,yerr=std,  fmt='o', ecolor='g', capthick=1.0)
 plt.title("standard deviation in idleness with varying runs and agents")
 plt.xlabel("number of agents")
-plt.ylabel("standard deviation in graph idleness")
-plt.legend()
+plt.ylabel("graph idleness")
+#plt.legend()
 # plt.show()
 plt.savefig('std_deviations.png', dpi = 100)
 # plt.savefig('dead'+str(dead)+'_new.png', dpi = 100)
