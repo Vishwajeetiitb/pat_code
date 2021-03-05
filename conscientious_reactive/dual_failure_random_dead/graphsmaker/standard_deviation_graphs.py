3049e28f51906789688ab32889f5a4b8bf26ed93
Dikshuy <dikshant1212hsr@gmail.com>
2021-03-05 12:58:30 +0530

    std

--- conscientious_reactive/dual_failure_random_dead/graphsmaker/standard_deviation_graphs.py
+++ conscientious_reactive/dual_failure_random_dead/graphsmaker/standard_deviation_graphs.py
@@ -3,12 +3,11 @@ import matplotlib.pyplot as plt
 import xlsxwriter
 import numpy as np
 import os
-rootdir ='../data/'
+rootdir ='../data_3/'
 
-cars = [1,2,3,4,5,6,7,8,9,10]
-deads = [0,2,4,6,8]
-num_runs = 10
-# avgs = []
+cars = [1,2,3,4,5,6,7,8,9,10,12]
+deads = [0]#,5,12,20,25]
+num_runs = 8
 plt.figure()
 
 for dead in deads:
@@ -27,14 +26,14 @@ for dead in deads:
 		avg.append(np.mean(graph_idlness))
 		std.append(np.std(graph_idlness))
 	print(std)
-	plt.plot(cars, std, label ="no of device failures ="+str(dead))
+	plt.plot(cars, std)#, label ="no of device failures ="+str(dead))
 	plt.draw()
 
 # plt.plot(cars, avg, 'b--')
 # for i in range(len(avgs)):
 # 	plt.plot(cars, avgs[i], label ="no of device failures ="+str(i*2))
 # plt.errorbar(cars, avg,yerr=std,  fmt='o', ecolor='g', capthick=1.0)
-plt.title("standard deviation in idleness with varying runs and agents with diff no of failed devices")
+plt.title("standard deviation in idleness with varying runs and agents")
 plt.xlabel("number of agents")
 plt.ylabel("standard deviation in graph idleness")
 plt.legend()
