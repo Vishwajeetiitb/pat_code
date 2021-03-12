1ec757cdec643e5ff267736cdd309aa571e1b696
Dikshuy <dikshant1212hsr@gmail.com>
2021-03-12 16:37:24 +0530

    plots added

--- conscientious_reactive/asymetric_map/graphsmaker/idleness_graphs.py
+++ conscientious_reactive/asymetric_map/graphsmaker/idleness_graphs.py
@@ -1,13 +1,13 @@
-	import pandas as pd
+import pandas as pd
 import matplotlib.pyplot as plt
 import xlsxwriter
 import numpy as np
 import os
-rootdir ='../data_2/'
+rootdir ='../data_3/'
 
-cars = [1,2,4,6,8,10,12]
-deads = [0,4,10,14,21,28]
-num_runs = 6
+cars =  [1,2, 5,8,12]
+deads = [0, 4, 10, 14, 21, 28]
+num_runs = 3
 plt.figure()
 
 for dead in deads:
@@ -18,7 +18,7 @@ for dead in deads:
 		runs = []
 		instantaneous_node_idleness =[]
 		for i in range(num_runs):
-			print(path+'run'+str(i)+'/run.xlsx')
+			# print(path+'run'+str(i)+'/run.xlsx')
 			runs.append(np.array(pd.read_excel(pd.ExcelFile(path+'run'+str(i)+'/run.xlsx'))))
 			runs[i] = runs[i][500:][:,1:]
 			instantaneous_node_idleness.append(np.mean(runs[i],axis=1))
@@ -26,15 +26,15 @@ for dead in deads:
 		avg.append(np.mean(graph_idlness))
 		# std.append(np.std(graph_idlness))
 	print(avg)
-	plt.plot(cars, avg, label ="no of device failures ="+str(dead))
+	plt.plot(cars, avg, label =str(dead)+" failures")
 	plt.draw()
 
 # plt.plot(cars, avg, 'b--')
 # for i in range(len(avgs)):
 # 	plt.plot(cars, avgs[i], label ="no of device failures ="+str(i*2))
 # plt.errorbar(cars, avg,yerr=std,  fmt='o', ecolor='g', capthick=1.0)
-plt.title("Idleness with varying runs and agents with diff no of failed devices")
-plt.xlabel("Number of agents")
+plt.title("Idleness with varying runs and agents")
+plt.xlabel("# agents")
 plt.ylabel("Graph Idleness")
 plt.legend()
 # plt.show()
