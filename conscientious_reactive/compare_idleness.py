e0fbc53cdb3d0d7e9698f5f44a872ec7f1b03eb7
Dikshuy <dikshant1212hsr@gmail.com>
2021-03-24 14:42:21 +0530

    changes

--- conscientious_reactive/compare_idleness.py
+++ conscientious_reactive/compare_idleness.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 import xlsxwriter
 import numpy as np
 import os
-rootdir1 ='/home/dikshant/github/pat_code/conscientious_reactive/dual_failure_random_dead/data_3/'
-rootdir2 = '/home/dikshant/github/pat_code/conscientious_reactive/dual_failure_random_dead_asymmetric_length/data/'
+rootdir1 ='/home/dikshant/github/pat_code/conscientious_reactive/dual_failure_random_dead_asymmetric_length/data/'
+rootdir2 = '/home/dikshant/github/pat_code/conscientious_reactive/dual_failure_random_dead_asymmetric_length/data_2/'
 
-cars =  [1, 2, 4, 5, 6, 7, 8, 10, 12]
-deads = [0,12,25]
-num_runs = 8
+cars =  [1,3,6,9,12]
+deads = [0, 12,25]
+num_runs = 3
 plt.figure()
 check = 0
 for dead in deads:
@@ -28,11 +28,11 @@ for dead in deads:
 		# std.append(np.std(graph_idlness))
 	print(avg)
 	if check == 0:
-		plt.plot(cars, avg, 'r-',label =str(dead)+" device failures (map A)")
+		plt.plot(cars, avg, 'r-',label =str(dead)+" without intent")
 	elif check == 1:
-		plt.plot(cars, avg, 'g-',label =str(dead)+" device failures (map A)")
+		plt.plot(cars, avg, 'g-',label =str(dead)+" without intent")
 	else:
-		plt.plot(cars, avg, 'b-',label =str(dead)+" device failures (map A)")
+		plt.plot(cars, avg, 'b-',label =str(dead)+" without intent")
 	check += 1
 	plt.draw()
 check = 0
@@ -53,23 +53,23 @@ for dead in deads:
 		# std.append(np.std(graph_idlness))
 	print(avg)
 	if check == 0:
-		plt.plot(cars, avg, 'r--',label =str(dead)+" device failures (map B)")
+		plt.plot(cars, avg, 'r--',label =str(dead)+" with intent")
 	elif check == 1:
-		plt.plot(cars, avg, 'g--',label =str(dead)+" device failures (map B)")
+		plt.plot(cars, avg, 'g--',label =str(dead)+" with intent")
 	else:
-		plt.plot(cars, avg, 'b--',label =str(dead)+" device failures (map B)")
+		plt.plot(cars, avg, 'b--',label =str(dead)+" with intent")
 	check += 1
 	plt.draw()
 # plt.plot(cars, avg, 'b--')
 # for i in range(len(avgs)):
 # 	plt.plot(cars, avgs[i], label ="no of device failures ="+str(i*2))
 # plt.errorbar(cars, avg,yerr=std,  fmt='o', ecolor='g', capthick=1.0)
-plt.title("Idleness comparison between maps A and B")
+plt.title("Intent comparison for Map C")
 plt.xlabel("# agents")
 plt.ylabel("Graph Idleness")
 plt.legend()
 # plt.show()
-plt.savefig('final.png', dpi = 100)
+plt.savefig('intent.png', dpi = 100)
 # plt.savefig('dead'+str(dead)+'_new.png', dpi = 100)
 	
 
